c9975723e7429e703d699c743e1e93c2ee5a45cf
Vikas Pandey <vikaspa6@gmail.com>
2022-04-05 09:06:01 +0800

    Create set-up.azure-cli.sh

---
+++ set-up.azure-cli.sh
@@ -0,0 +1,10 @@
+## SetUp Azure-Cli
+
+#----Install dependencies
+apk add gcc musl-dev python3-dev libffi-dev openssl-dev cargo make
+
+#----Install python-pip
+apk add py3-pip
+
+#----Install azure-Cli
+pip install azure-cli
